7b01821d8e18e621859925cde90e22d8b7c651be
nidhitiwari9136 <nidhitiwari913655@gmail.com>
2026-03-01 21:35:53 +0530

    api add

--- src/api.jsx
+++ src/api.jsx
@@ -5,7 +5,12 @@ const API = axios.create({
   baseURL: "https://emotitales-backend.onrender.com/api/",
 });
 
-export const getSummary = (text) => API.post("summary/", { text });
-export const generateStory = (formData) => API.post("story/", formData);
-
+// export const getSummary = (text) => API.post("summary/", { text });
+// export const generateStory = (formData) => API.post("story/", formData);
+export const loginUser = (data) => API.post("login/", data);
+export const registerUser = (data) => API.post("register/", data);
+export const getSummary = (formData) => API.post("summary/", formData); // PDF hai toh formData bhejna
+export const generateStory = (data) => API.post("story/", data);
+export const getLibrary = () => API.get("library/");
+export const getAudio = (storyText) => API.post("audio/", { story: storyText });
 export default API;
